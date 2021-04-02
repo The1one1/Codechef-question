d21313d0a4cb8e5274d27e2174b6a6ddb455bd02
The One <rahulsharma22459@gmail.com>
2021-04-02 21:02:45 +0530

    codechef march long challenge first question

---
+++ valid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	short a,b,c;
+	cin>>a>>b>>c;
+	if((a==b)||(b==c)||(c==a))
+	{
+	    cout<<"YES\n";
+	}
+	else
+	cout<<"NO\n";
+	return 0;
+}
+
